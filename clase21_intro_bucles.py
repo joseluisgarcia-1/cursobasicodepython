8e5d022e20d75d0c3451af4c470a9de775950b6c
joseluisgarcia-1 <jlgarcia002@misena.edu.co>
2022-03-25 15:57:39 -0500

    agregando archivos de la clase 21 y 22 de intro a bucles

---
+++ clase21_intro_bucles.py
@@ -0,0 +1,15 @@
+contador = 0
+print('2 elevado a '+ str(contador)+' es igual a: '+str(2**contador))
+contador = 1
+print('2 elevado a '+ str(contador)+' es igual a: '+str(2**contador))
+contador = 2
+print('2 elevado a '+ str(contador)+' es igual a: '+str(2**contador))
+contador = 3
+print('2 elevado a '+ str(contador)+' es igual a: '+str(2**contador))
+contador = 4
+print('2 elevado a '+ str(contador)+' es igual a: '+str(2**contador))
+contador = 5
+print('2 elevado a '+ str(contador)+' es igual a: '+str(2**contador))
+contador = 6
+print('2 elevado a '+ str(contador)+' es igual a: '+str(2**contador))
+print("para evitar hacer eso así, vamos a hacer uso de los bucles que nos facilita todas esas líneas")
